6d163bb7956e7258e19fd12f9e247baf893f2c68
Yskinator <yski2moreletters@gmail.com>
2017-05-24 00:10:21 +0300

    Implemented flower drawing. Positions still need adjusting.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  get 'suppliers/:id/flower' => 'suppliers#flower'
+  get 'suppliers/:id/flower' => 'suppliers#flower', as: :supplier_flower
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
